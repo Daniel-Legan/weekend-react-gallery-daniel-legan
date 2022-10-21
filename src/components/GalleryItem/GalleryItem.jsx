15008608217e8de08f19e06b0334d4d32722f887
Daniel Legan <legan.daniel@gmail.com>
2022-10-21 13:35:58 -0500

    clean-up output

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,10 +1,12 @@
 function GalleryItem({ item }) {
     return (
         <>
-            <li>{item.id}</li>
-            <img src={item.path} />
-            <li>{item.description}</li>
-            <li>{item.likes}</li>
+            <div>
+                {/* <li>{item.id}</li> */}
+                <img src={item.path} />
+                {/* <li>{item.description}</li> */}
+                <div>{item.likes} people love this</div>
+            </div>
         </>
     );
 }
